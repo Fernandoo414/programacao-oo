0f80ad59d139442a437bf5d5ab202a9e657979b6
Fernando Bach <fernandobach02@email.com>
2026-03-31 01:44:42 -0300

    resolvendo conflito da minha versao

--- questao16/main.dart
+++ questao16/main.dart
@@ -1,6 +1,5 @@
 import 'personagem.dart';
 
-<<<<<<< HEAD
 void main(){
 
     var p1 = Personagem('guerreiro', 100, 25);
@@ -18,29 +17,4 @@ void main(){
 
         print('---------');
     }
-=======
-void main() {
-  Personagem p1 = Personagem("Guerreiro", 100, 20);
-  Personagem p2 = Personagem("Mago", 80, 25);
-
-  while (p1.estaVivo() && p2.estaVivo()) {
-    p1.atacar(p2);
-    if (p2.estaVivo()) {
-      p2.atacar(p1);
-    }
-
-    print("");
-    p1.exibirStatus();
-    p2.exibirStatus();
-    print("----------------------\n");
-  }
-
-  print("=== FIM DO COMBATE ===");
-
-  if (p1.estaVivo()) {
-    print("${p1.nome} venceu!");
-  } else {
-    print("${p2.nome} venceu!");
-  }
->>>>>>> 1708ae13995ff323d90c3d712c2fb31e8c4f5e4e
 }
--- questao16/personagem.dart
+++ questao16/personagem.dart
@@ -1,5 +1,4 @@
 class Personagem {
-<<<<<<< HEAD
       String nome;
       int vida;
       int ataque;
@@ -41,49 +40,4 @@ class Personagem {
             print("Vida: $vida");
             print("Ataque: $ataque");
       }
-=======
-  String nome;
-  int vida;
-  int ataque;
-
-  Personagem(this.nome, this.vida, this.ataque) {
-    if (nome.isEmpty) {
-      throw ArgumentError("O nome não pode ser vazio.");
-    }
-    if (vida <= 0) {
-      throw ArgumentError("A vida deve ser maior que 0.");
-    }
-    if (ataque <= 0) {
-      throw ArgumentError("O ataque deve ser maior que 0.");
-    }
-  }
-
-  bool estaVivo() {
-    return vida > 0;
-  }
-
-  void atacar(Personagem inimigo) {
-    if (!estaVivo()) {
-      print("$nome não pode atacar porque está morto.");
-      return;
-    }
-
-    if (!inimigo.estaVivo()) {
-      print("$nome tentou atacar ${inimigo.nome}, mas ele já está morto.");
-      return;
-    }
-
-    inimigo.vida -= ataque;
-
-    if (inimigo.vida < 0) {
-      inimigo.vida = 0;
-    }
-
-    print("$nome atacou ${inimigo.nome} causando $ataque de dano!");
-  }
-
-  void exibirStatus() {
-    print("Nome: $nome | Vida: $vida | Ataque: $ataque");
-  }
->>>>>>> 1708ae13995ff323d90c3d712c2fb31e8c4f5e4e
 }
