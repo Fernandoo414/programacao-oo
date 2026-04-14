e74830bf00d573e8b8470611b9840a043d677a37
Fernandoo414 <fernandobach02@gmail.com>
2026-04-13 22:13:11 -0300

    Update main.dart

--- estudo-dirigido-1.2/estudo-dirigido-1.2/lib/questao05/main.dart
+++ estudo-dirigido-1.2/estudo-dirigido-1.2/lib/questao05/main.dart
@@ -1,17 +1,17 @@
 import 'conta.dart';
 
 void main() {
-  var conta1 = Conta.bancaria("123", "Caio");
-  var conta2 = Conta.vip("456", "Lucas", 1000);
+  var conta1 = Conta.bancaria("321", "Lucas");
+  var conta2 = Conta.vip("456", "Leonardo", 1000);
 
   conta1.exibirResumo();
   conta2.exibirResumo();
 
-  conta1.depositar(200);
-  conta1.sacar(50);
+  conta1.depositar(300);
+  conta1.sacar(60);
 
-  conta2.sacar(300);
-  conta2.depositar(500);
+  conta2.sacar(600);
+  conta2.depositar(400);
 
   conta1.exibirResumo();
   conta2.exibirResumo();
