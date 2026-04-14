ff371d032ac69c21b53ef23a7ff7669d12b8e637
Fernandoo414 <fernandobach02@gmail.com>
2026-04-13 22:11:11 -0300

    Update main.dart

--- estudo-dirigido-1.2/estudo-dirigido-1.2/lib/questao03/main.dart
+++ estudo-dirigido-1.2/estudo-dirigido-1.2/lib/questao03/main.dart
@@ -2,8 +2,8 @@ import 'aluno.dart';
 
 void main() {
   var a1 = Aluno(ra: 1, nome: "Leonardo", notaFinal: 7.5);
-  var a2 = Aluno(ra: 2, nome: "Lucas", notaFinal: 5.0);
-  var a3 = Aluno(ra: 3, nome: "Fernando", notaFinal: 9.0);
+  var a2 = Aluno(ra: 2, nome: "Lucas", notaFinal: 4.0);
+  var a3 = Aluno(ra: 3, nome: "Fernando", notaFinal: 8.0);
 
   a1.exibirBoletim();
   a2.exibirBoletim();
